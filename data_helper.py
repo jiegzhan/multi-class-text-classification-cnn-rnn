09139c2fe1edaf6f742edb5b218abbb2ad8b2623
Jie Zhang <jiegzhan@gmail.com>
2016-11-04 12:36:09 -0700

    Load embeddings.

--- data_helper.py
+++ data_helper.py
@@ -3,6 +3,7 @@ import re
 import sys
 import json
 import pickle
+import logging
 import itertools
 import numpy as np
 import pandas as pd
@@ -10,6 +11,8 @@ import gensim as gs
 from pprint import pprint
 from collections import Counter
 from tensorflow.contrib import learn
+
+logging.getLogger().setLevel(logging.INFO)
 
 def clean_str(s):
 	s = re.sub(r"[^A-Za-z0-9:(),!?\'\`]", " ", s)
@@ -28,68 +31,62 @@ def clean_str(s):
 	s = re.sub(r"\s{2,}", " ", s)
 	return s.strip().lower()
 
-def load_trained_vecs(vocabulary):
-	model = gs.models.Word2Vec.load_word2vec_format('./data/GoogleNews-vectors-negative300.bin', binary=True)
-	trained_vecs = {}
+def load_embeddings(vocabulary):
+	google_embeddings = gs.models.Word2Vec.load_word2vec_format('./data/GoogleNews-vectors-negative300.bin', binary=True)
+	word_embeddings = {}
 	for word in vocabulary:
-		if word in model:
-			trained_vecs[word] = model[word]
-	return trained_vecs
-
-def add_unknown_words(word_vecs, vocab, min_df=0, k=300):
-	for word in vocab:
-		if word not in word_vecs and vocab[word] >= min_df:
-			word_vecs[word] = np.random.uniform(-0.25, 0.25, k)
+		if word in google_embeddings:
+			word_embeddings[word] = google_embeddings[word]
+		else:
+			word_embeddings[word] = np.random.uniform(-0.25, 0.25, 300)
+	return word_embeddings
 
 def pad_sentences(sentences, padding_word="<PAD/>", params=None):
-	"""Padding setences during training or prediction"""
+	"""Pad setences during training or prediction"""
 	if params is None: # Train
 		sequence_length = max(len(x) for x in sentences)
 	else: # Prediction
-		print('This is prediction, reading the trained sequence length')
+		logging.critical('This is prediction, reading the trained sequence length')
 		sequence_length = params['sequence_length']
 
-	print('The maximum length is {}'.format(sequence_length))
+	logging.critical('The maximum length is {}'.format(sequence_length))
 	padded_sentences = []
 	for i in range(len(sentences)):
 		sentence = sentences[i]
 		num_padding = sequence_length - len(sentence)
 
 		if num_padding < 0: # Prediction: cut off the sentence if it is longer than the sequence length
-			print('This sentence has to be cut off because it is longer than trained sequence length')
+			logging.info('This sentence has to be cut off because it is longer than trained sequence length')
 			padded_sentence = sentence[0:sequence_length]
 		else:
 			padded_sentence = sentence + [padding_word] * num_padding
-
 		padded_sentences.append(padded_sentence)
 	return padded_sentences
 
 def build_vocab(sentences):
 	word_counts = Counter(itertools.chain(*sentences))
-	vocabulary_inv = [x[0] for x in word_counts.most_common()]
-	vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
-	return [vocabulary, vocabulary_inv]
+	vocabulary_inv = [word[0] for word in word_counts.most_common()]
+	vocabulary = {word: index for index, word in enumerate(vocabulary_inv)}
+	return vocabulary, vocabulary_inv
 
-def batch_iter(data, batch_size, num_epochs, predict=False):
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
 	data = np.array(data)
 	data_size = len(data)
-	num_batches_per_epoch = int(len(data) / batch_size) + 1
+	num_batches_per_epoch = int(data_size / batch_size) + 1
 
 	for epoch in range(num_epochs):
-		if predict is False: # Shuffle the data during training and dev step
+		if shuffle:
 			shuffle_indices = np.random.permutation(np.arange(data_size))
 			shuffled_data = data[shuffle_indices]
-		else: # Do not shuffle the data during prediction step
+		else:
 			shuffled_data = data
 
 		for batch_num in range(num_batches_per_epoch):
 			start_index = batch_num * batch_size
-			end_index = (batch_num + 1) * batch_size
-			if end_index > data_size:
-				end_index = data_size
+			end_index = min((batch_num + 1) * batch_size, data_size)
 			yield shuffled_data[start_index:end_index]
 
-def load_data_and_labels(filename):
+def load_data(filename):
 	df = pd.read_csv(filename, sep='|')
 	selected = ['PROPOSED_CATEGORY', 'DESCRIPTION_UNMASKED']
 	non_selected = list(set(df.columns) - set(selected))
@@ -102,32 +99,17 @@ def load_data_and_labels(filename):
 	nb_labels = len(labels)
 	one_hot = np.zeros((nb_labels, nb_labels), int)
 	np.fill_diagonal(one_hot, 1)
-
 	label_dict = dict(zip(labels, one_hot))
-	for key in sorted(label_dict.keys()):
-		print('{} ---> {}'.format(key, label_dict[key]))
 
-	y = df[selected[0]].apply(lambda x: label_dict[x]).tolist()
-	examples = df[selected[1]].apply(lambda x: clean_str(x).split(' ')).tolist()
-	print(len(y))
-	print(len(examples))
-	for i in range(10):
-		print('{} ---> {}'.format(y[i], examples[i]))
-	return [examples, y, df, labels]
+	x_raw= df[selected[1]].apply(lambda x: clean_str(x).split(' ')).tolist()
+	y_raw = df[selected[0]].apply(lambda y: label_dict[y]).tolist()
 
-def build_input_data(sentences, labels, vocabulary):
-	x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
-	print("x are {}".format(x))
-	y = np.array(labels)
-	print("y are {}".format(y))
-	return [x, y]
+	x_raw = pad_sentences(x_raw)
+	vocabulary, vocabulary_inv = build_vocab(x_raw)
 
-def load_data(filename):
-	sentences, labels, df, labels_list = load_data_and_labels(filename)
-	sentences_padded = pad_sentences(sentences)
-	vocabulary, vocabulary_inv = build_vocab(sentences_padded)
-	x, y = build_input_data(sentences_padded, labels, vocabulary)
-	return [x, y, vocabulary, vocabulary_inv, df, labels_list]
+	x = np.array([[vocabulary[word] for word in sentence] for sentence in x_raw])
+	y = np.array(y_raw)
+	return x, y, vocabulary, vocabulary_inv, df, labels
 
 if __name__ == "__main__":
 	train_file = './data/bank_debit/input_40000.csv'
