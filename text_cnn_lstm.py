88563d89a5e0e38515c97aa47e029c5226e2ed94
Jie Zhang <jiegzhan@gmail.com>
2016-11-04 14:13:53 -0700

    Calculate the actual length

--- text_cnn_lstm.py
+++ text_cnn_lstm.py
@@ -5,28 +5,28 @@ class TextCNNLSTM(object):
 	def __init__(self, embedding_mat, non_static, hidden_unit, sequence_length, max_pool_size,
 		num_classes, embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
 
-		self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-		self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-		self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+		self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name='input_x')
+		self.input_y = tf.placeholder(tf.float32, [None, num_classes], name='input_y')
+		self.dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
 		self.batch_size = tf.placeholder(tf.int32)
-		self.pad = tf.placeholder(tf.float32, [None, 1, embedding_size, 1], name="pad")
-		self.real_len = tf.placeholder(tf.int32, [None], name="real_len")
+		self.pad = tf.placeholder(tf.float32, [None, 1, embedding_size, 1], name='pad')
+		self.real_len = tf.placeholder(tf.int32, [None], name='real_len')
 
 		l2_loss = tf.constant(0.0)
 
-		with tf.device('/cpu:0'), tf.name_scope("embedding"):
+		with tf.device('/cpu:0'), tf.name_scope('embedding'):
 			if not non_static:
-				W = tf.constant(embedding_mat, name="W")
+				W = tf.constant(embedding_mat, name='W')
 			else:
-				W = tf.Variable(embedding_mat, name="W")
+				W = tf.Variable(embedding_mat, name='W')
 			self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
 			emb = tf.expand_dims(self.embedded_chars, -1)
 
 		pooled_concat = []
-		reduced = np.int32(np.ceil((sequence_length)*1.0/max_pool_size))
+		reduced = np.int32(np.ceil((sequence_length) * 1.0 / max_pool_size))
 
 		for i, filter_size in enumerate(filter_sizes):
-			with tf.name_scope("conv-maxpool-%s" % filter_size):
+			with tf.name_scope('conv-maxpool-%s' % filter_size):
 
 				# Zero paddings so that the convolution output have dimension batch x sequence_length x emb_size x channel
 				num_prio = (filter_size-1) // 2
@@ -36,22 +36,20 @@ class TextCNNLSTM(object):
 				emb_pad = tf.concat(1, [pad_prio, emb, pad_post])
 
 				filter_shape = [filter_size, embedding_size, 1, num_filters]
-				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-				conv = tf.nn.conv2d(emb_pad, W, strides=[1, 1, 1, 1], padding="VALID", name="conv")
+				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
+				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name='b')
+				conv = tf.nn.conv2d(emb_pad, W, strides=[1, 1, 1, 1], padding='VALID', name='conv')
 
-				h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+				h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
 
 				# Maxpooling over the outputs
-				pooled = tf.nn.max_pool(h, ksize=[1, max_pool_size, 1, 1], strides=[1, max_pool_size, 1, 1], padding='SAME', name="pool")
+				pooled = tf.nn.max_pool(h, ksize=[1, max_pool_size, 1, 1], strides=[1, max_pool_size, 1, 1], padding='SAME', name='pool')
 				pooled = tf.reshape(pooled, [-1, reduced, num_filters])
 				pooled_concat.append(pooled)
 
 		pooled_concat = tf.concat(2, pooled_concat)
 		pooled_concat = tf.nn.dropout(pooled_concat, self.dropout_keep_prob)
 
-		# GRU VS LSTM
-		# lstm_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_unit)
 		lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_unit)
 		lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=self.dropout_keep_prob)
 
@@ -61,7 +59,7 @@ class TextCNNLSTM(object):
 
 		# Collect the appropriate last words into variable output (dimension = batch x embedding_size)
 		output = outputs[0]
-		with tf.variable_scope("Output"):
+		with tf.variable_scope('Output'):
 			tf.get_variable_scope().reuse_variables()
 			one = tf.ones([1, hidden_unit], tf.float32)
 			for i in range(1,len(outputs)):
@@ -71,22 +69,22 @@ class TextCNNLSTM(object):
 				mat = tf.matmul(ind, one)
 				output = tf.add(tf.mul(output, mat),tf.mul(outputs[i], 1.0 - mat))
 
-		with tf.name_scope("output"):
-			self.W = tf.Variable(tf.truncated_normal([hidden_unit, num_classes], stddev=0.1), name="W")
-			b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+		with tf.name_scope('output'):
+			self.W = tf.Variable(tf.truncated_normal([hidden_unit, num_classes], stddev=0.1), name='W')
+			b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name='b')
 			l2_loss += tf.nn.l2_loss(W)
 			l2_loss += tf.nn.l2_loss(b)
-			self.scores = tf.nn.xw_plus_b(output, self.W, b, name="scores")
-			self.predictions = tf.argmax(self.scores, 1, name="predictions")
+			self.scores = tf.nn.xw_plus_b(output, self.W, b, name='scores')
+			self.predictions = tf.argmax(self.scores, 1, name='predictions')
 
-		with tf.name_scope("loss"):
+		with tf.name_scope('loss'):
 			losses = tf.nn.softmax_cross_entropy_with_logits(self.scores, self.input_y)
 			self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
-		with tf.name_scope("accuracy"):
+		with tf.name_scope('accuracy'):
 			correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-			self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+			self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name='accuracy')
 
-		with tf.name_scope("num_correct"):
+		with tf.name_scope('num_correct'):
 			correct = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-			self.num_correct = tf.reduce_sum(tf.cast(correct, "float"))
+			self.num_correct = tf.reduce_sum(tf.cast(correct, 'float'))
