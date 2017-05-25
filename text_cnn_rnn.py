8011a92454dd8b2e6dfb48b2e88c4885cedbc448
gustavomr <gustavomr@users.noreply.github.com>
2017-05-25 10:33:47 -0300

    Update text_cnn_rnn.py
    make it compatible with TF 1.x

--- text_cnn_rnn.py
+++ text_cnn_rnn.py
@@ -24,16 +24,16 @@ class TextCNNRNN(object):
 
 		pooled_concat = []
 		reduced = np.int32(np.ceil((sequence_length) * 1.0 / max_pool_size))
-
+		
 		for i, filter_size in enumerate(filter_sizes):
 			with tf.name_scope('conv-maxpool-%s' % filter_size):
 
 				# Zero paddings so that the convolution output have dimension batch x sequence_length x emb_size x channel
 				num_prio = (filter_size-1) // 2
 				num_post = (filter_size-1) - num_prio
-				pad_prio = tf.concat(1, [self.pad] * num_prio)
-				pad_post = tf.concat(1, [self.pad] * num_post)
-				emb_pad = tf.concat(1, [pad_prio, emb, pad_post])
+				pad_prio = tf.concat([self.pad] * num_prio,1)
+				pad_post = tf.concat([self.pad] * num_post,1)
+				emb_pad = tf.concat([pad_prio, emb, pad_post],1)
 
 				filter_shape = [filter_size, embedding_size, 1, num_filters]
 				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
@@ -47,16 +47,23 @@ class TextCNNRNN(object):
 				pooled = tf.reshape(pooled, [-1, reduced, num_filters])
 				pooled_concat.append(pooled)
 
-		pooled_concat = tf.concat(2, pooled_concat)
+		pooled_concat = tf.concat(pooled_concat,2)
 		pooled_concat = tf.nn.dropout(pooled_concat, self.dropout_keep_prob)
 
 		# lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_unit)
-		lstm_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_unit)
-		lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=self.dropout_keep_prob)
+
+		#lstm_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_unit)
+		lstm_cell = tf.contrib.rnn.GRUCell(num_units=hidden_unit)
+
+		#lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=self.dropout_keep_prob)
+		lstm_cell = tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=self.dropout_keep_prob)
+		
 
 		self._initial_state = lstm_cell.zero_state(self.batch_size, tf.float32)
-		inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, reduced, pooled_concat)]
-		outputs, state = tf.nn.rnn(lstm_cell, inputs, initial_state=self._initial_state, sequence_length=self.real_len)
+		#inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, reduced, pooled_concat)]
+		inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(pooled_concat,num_or_size_splits=int(reduced),axis=1)]
+		#outputs, state = tf.nn.rnn(lstm_cell, inputs, initial_state=self._initial_state, sequence_length=self.real_len)
+		outputs, state = tf.contrib.rnn.static_rnn(lstm_cell, inputs, initial_state=self._initial_state, sequence_length=self.real_len)
 
 		# Collect the appropriate last words into variable output (dimension = batch x embedding_size)
 		output = outputs[0]
@@ -68,7 +75,7 @@ class TextCNNRNN(object):
 				ind = tf.to_float(ind)
 				ind = tf.expand_dims(ind, -1)
 				mat = tf.matmul(ind, one)
-				output = tf.add(tf.mul(output, mat),tf.mul(outputs[i], 1.0 - mat))
+				output = tf.add(tf.multiply(output, mat),tf.multiply(outputs[i], 1.0 - mat))
 
 		with tf.name_scope('output'):
 			self.W = tf.Variable(tf.truncated_normal([hidden_unit, num_classes], stddev=0.1), name='W')
@@ -79,7 +86,7 @@ class TextCNNRNN(object):
 			self.predictions = tf.argmax(self.scores, 1, name='predictions')
 
 		with tf.name_scope('loss'):
-			losses = tf.nn.softmax_cross_entropy_with_logits(self.scores, self.input_y)
+			losses = tf.nn.softmax_cross_entropy_with_logits(labels = self.input_y, logits = self.scores) #  only named arguments accepted            
 			self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
 		with tf.name_scope('accuracy'):
